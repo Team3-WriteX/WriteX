848859875d7c73a80429867a57305089358cabe3
Ines Agasaro <ines.ituze@ashesi.edu.gh>
2021-11-02 17:52:33 +0000

    Front End Editing

---
+++ menu.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--  
+    Document Title
+    =============================================
+    -->
+    <title>WriteX | Proffessional Writing </title>
+    <!--  
+    Favicons
+    =============================================
+    -->
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/images/favicons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="assets/images/favicons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/images/favicons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/images/favicons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/images/favicons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/images/favicons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/images/favicons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/images/favicons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="assets/images/favicons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/images/favicons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicons/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="assets/images/favicons/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <!--  
+    Stylesheets
+    =============================================
+    
+    -->
+    <!-- Default stylesheets-->
+    <link href="assets/lib/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Template specific stylesheets-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Volkhov:400i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+    <link href="assets/lib/animate.css/animate.css" rel="stylesheet">
+    <link href="assets/lib/components-font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/lib/et-line-font/et-line-font.css" rel="stylesheet">
+    <link href="assets/lib/flexslider/flexslider.css" rel="stylesheet">
+    <link href="assets/lib/owl.carousel/dist/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/lib/owl.carousel/dist/assets/owl.theme.default.min.css" rel="stylesheet">
+    <link href="assets/lib/magnific-popup/dist/magnific-popup.css" rel="stylesheet">
+    <link href="assets/lib/simple-text-rotator/simpletextrotator.css" rel="stylesheet">
+    <!-- Main stylesheet and color file-->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link id="color-scheme" href="assets/css/colors/default.css" rel="stylesheet">
+  </head>
+  <body data-spy="scroll" data-target=".onpage-navigation" data-offset="60">
+    <main>
+      <div class="page-loader">
+        <div class="loader">Loading...</div>
+      </div>
+      <nav class="navbar navbar-custom navbar-fixed-top navbar-transparent" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#custom-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="index.html">Titan</a>
+          </div>
+          <div class="collapse navbar-collapse" id="custom-collapse">
+            <ul class="nav navbar-nav navbar-right">
+             
+            
+        
+                  <li ><a  href="service1.php" >Services</a>
+                    
+                  </li>
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Pricing</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="pricing1.html">Pricing 1</a></li>
+                      <li><a href="pricing2.html">Pricing 2</a></li>
+                    </ul>
+                  </li>
+                
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Contact</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="contact1.html">Contact 1</a></li>
+                      <li><a href="contact2.html">Contact 2</a></li>
+                      <li><a href="contact3.html">Contact 3</a></li>
+                    </ul>
+                  </li>
+                
+                  <li><a href="login_register.html">Login / Register</a></li>
+                  <li><a href="faq.html">FAQ</a></li>
+                 
+               
+             
+              <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Testimonials</a>
+                <ul class="dropdown-menu" role="menu">
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Boxed</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="portfolio_boxed_col_2.html">2 Columns</a></li>
+                      <li><a href="portfolio_boxed_col_3.html">3 Columns</a></li>
+                      <li><a href="portfolio_boxed_col_4.html">4 Columns</a></li>
+                    </ul>
+                  </li>
+         
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Full Width</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="portfolio_full_width_col_2.html">2 Columns</a></li>
+                      <li><a href="portfolio_full_width_col_3.html">3 Columns</a></li>
+                      <li><a href="portfolio_full_width_col_4.html">4 Columns</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Full Width - Gutter</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="portfolio_full_width_gutter_col_2.html">2 Columns</a></li>
+                      <li><a href="portfolio_full_width_gutter_col_3.html">3 Columns</a></li>
+                      <li><a href="portfolio_full_width_gutter_col_4.html">4 Columns</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Masonry</a>
+                    <ul class="dropdown-menu">
+                      <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Boxed</a>
+                        <ul class="dropdown-menu">
+                          <li><a href="portfolio_masonry_boxed_col_2.html">2 Columns</a></li>
+                          <li><a href="portfolio_masonry_boxed_col_3.html">3 Columns</a></li>
+                          <li><a href="portfolio_masonry_boxed_col_4.html">4 Columns</a></li>
+                        </ul>
+                      </li>
+                      <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Full Width</a>
+                        <ul class="dropdown-menu">
+                          <li><a href="portfolio_masonry_full_width_col_2.html">2 Columns</a></li>
+                          <li><a href="portfolio_masonry_full_width_col_3.html">3 Columns</a></li>
+                          <li><a href="portfolio_masonry_full_width_col_4.html">4 Columns</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Hover Style</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="portfolio_hover_black.html">Black</a></li>
+                      <li><a href="portfolio_hover_gradient.html">Gradient</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Single</a>
+                    <ul class="dropdown-menu">
+                      <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Featured Image</a>
+                        <ul class="dropdown-menu">
+                          <li><a href="portfolio_single_featured_image1.html">Style 1</a></li>
+                          <li><a href="portfolio_single_featured_image2.html">Style 2</a></li>
+                        </ul>
+                      </li>
+                      <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Featured Slider</a>
+                        <ul class="dropdown-menu">
+                          <li><a href="portfolio_single_featured_slider1.html">Style 1</a></li>
+                          <li><a href="portfolio_single_featured_slider2.html">Style 2</a></li>
+                        </ul>
+                      </li>
+                      <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Featured Video</a>
+                        <ul class="dropdown-menu">
+                          <li><a href="portfolio_single_featured_video1.html">Style 1</a></li>
+                          <li><a href="portfolio_single_featured_video2.html">Style 2</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              
+              
+              <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Shop</a>
+                <ul class="dropdown-menu" role="menu">
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Product</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="shop_product_col_3.html">3 columns</a></li>
+                      <li><a href="shop_product_col_4.html">4 columns</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="shop_single_product.html">Single Product</a></li>
+                  <li><a href="shop_checkout.html">Checkout</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      </html>
