c028405f8c5f125e0d4162e1580493ea8bb2b794
Ines Agasaro <ines.ituze@ashesi.edu.gh>
2021-11-03 13:26:28 +0000

    front end landing page done

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
 <div class="module-small bg-dark">
           <div class="container">
             <div class="row">
-              <div class="col-sm-3">
+              <div class="col-md-6 col-lg-6 ">
                 <div class="widget">
-                  <h5 class="widget-title font-alt">About Titan</h5>
-                  <p>The languages only differ in their grammar, their pronunciation and their most common words.</p>
-                  <p>Phone: +1 234 567 89 10</p>Fax: +1 234 567 89 10
-                  <p>Email:<a href="#">somecompany@example.com</a></p>
+                  <h5 class="widget-title font-alt">About WriteX</h5>
+                  <p> providing the excellent academic & professional writing services.</p>
+                  <p>Phone: +2330209245687</p>
+                  <p>Email:<a href="#">support.writex@gmail.com</a></p>
                 </div>
               </div>
-              <div class="col-sm-3">
+              <div class="col-md-6 col-lg-6">
                 <div class="widget">
                   <h5 class="widget-title font-alt">Recent Comments</h5>
                   <ul class="icon-list">
@@ -21,39 +21,7 @@
                   </ul>
                 </div>
               </div>
-              <div class="col-sm-3">
-                <div class="widget">
-                  <h5 class="widget-title font-alt">Blog Categories</h5>
-                  <ul class="icon-list">
-                    <li><a href="#">Photography - 7</a></li>
-                    <li><a href="#">Web Design - 3</a></li>
-                    <li><a href="#">Illustration - 12</a></li>
-                    <li><a href="#">Marketing - 1</a></li>
-                    <li><a href="#">Wordpress - 16</a></li>
-                  </ul>
-                </div>
-              </div>
-              <div class="col-sm-3">
-                <div class="widget">
-                  <h5 class="widget-title font-alt">Popular Posts</h5>
-                  <ul class="widget-posts">
-                    <li class="clearfix">
-                      <div class="widget-posts-image"><a href="#"><img src="assets/images/rp-1.jpg" alt="Post Thumbnail"/></a></div>
-                      <div class="widget-posts-body">
-                        <div class="widget-posts-title"><a href="#">Designer Desk Essentials</a></div>
-                        <div class="widget-posts-meta">23 january</div>
-                      </div>
-                    </li>
-                    <li class="clearfix">
-                      <div class="widget-posts-image"><a href="#"><img src="assets/images/rp-2.jpg" alt="Post Thumbnail"/></a></div>
-                      <div class="widget-posts-body">
-                        <div class="widget-posts-title"><a href="#">Realistic Business Card Mockup</a></div>
-                        <div class="widget-posts-meta">15 February</div>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+            
             </div>
           </div>
         </div>
@@ -62,12 +30,9 @@
           <div class="container">
             <div class="row">
               <div class="col-sm-6">
-                <p class="copyright font-alt">&copy; 2017&nbsp;<a href="index.html">TitaN</a>, All Rights Reserved</p>
+                <p class="copyright font-alt">&copy; 2017&nbsp;<a href="index.php">WriteX</a>, All Rights Reserved</p>
               </div>
-              <div class="col-sm-6">
-                <div class="footer-social-links"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-dribbble"></i></a><a href="#"><i class="fa fa-skype"></i></a>
-                </div>
-              </div>
+             
             </div>
           </div>
         </footer>
--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require_once("menu.php");
               <div class="titan-caption">
                 <div class="caption-content">
                   <div class="font-alt mb-30 titan-title-size-1"> Hello & welcome</div>
-                  <div class="font-alt mb-40 titan-title-size-4">Professional & Academic Writing Made Easy!</div><a class="section-scroll btn btn-border-w btn-round" href="#menu">Request For a Quote</a>
+                  <div class="font-alt mb-40 titan-title-size-4">Professional & Academic Writing Made Easy!</div><a class="section-scroll btn btn-border-w btn-round" href="#quote">Request For a Quote</a>
                 </div>
               </div>
             </li>
@@ -20,7 +20,7 @@ require_once("menu.php");
               <div class="titan-caption">
                 <div class="caption-content">
                   <div class="font-alt mb-30 titan-title-size-1"> Take a look at</div>
-                  <div class="font-alt mb-40 titan-title-size-3">our services</div><a class="section-scroll btn btn-border-w btn-round" href="#specialities">Learn More</a>
+                  <div class="font-alt mb-40 titan-title-size-3">our services</div><a class="section-scroll btn btn-border-w btn-round" href="#services">Learn More</a>
                 </div>
               </div>
             </li>
@@ -30,10 +30,10 @@ require_once("menu.php");
       <div class="main">
         <section class="module" id="services">
           <div class="container">
-            <div class="row">
-              <div class="col-sm-2 col-sm-offset-5">
+            <div class="row ">
+              <div class="col-sm-2 col-sm-offset-4">
                 <div class="alt-module-subtitle"><span class="holder-w"></span>
-                  <h5 class="font-serif">For your comfort</h5><span class="holder-w"></span>
+                  <h5 class="font-serif" style= "text-align:center">Best Editing and Proofreading Services at Your Fingertips</h5><span class="holder-w"></span>
                 </div>
               </div>
             </div>
@@ -42,31 +42,21 @@ require_once("menu.php");
                 <h2 class="module-title font-alt">Our Services</h2>
               </div>
             </div>
-            <div class="row multi-columns-row">
-              <div class="col-sm-6 col-md-3 col-lg-3">
+            <div class="row d-flex justify-content-center text-center" >
+              <div class="col-md-6 col-lg-6">
                 <div class="features-item">
-                  <div class="features-icon"><span class="icon-clock"></span></div>
-                  <h3 class="features-title font-alt">Opened 24/7</h3>Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.
+                  <div class="features-icon"><span class="icon-documents"></span></div>
+                  <h3 class="features-title font-alt">Academic Writing</h3> We help your work reach its potential. In addition, our experts can help you free up time and energy by formatting your citations and references according to the style guide of your choice as well as proof read your work.
                 </div>
               </div>
-              <div class="col-sm-6 col-md-3 col-lg-3">
+              <div class="col-md-6 col-lg-6">
                 <div class="features-item">
-                  <div class="features-icon"><span class="icon-streetsign"></span></div>
-                  <h3 class="features-title font-alt">Free parking</h3>Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.
+                  <div class="features-icon"><span class="icon-edit"></span></div>
+                  <h3 class="features-title font-alt">Professional Writing</h3>We provide affordable resume and cover letter editors with the competitive advantage it needs to go straight to the top of the interview list. Our expert editors will ensure that your strengths and most relevant work experiences are highlighted.
                 </div>
               </div>
-              <div class="col-sm-6 col-md-3 col-lg-3">
-                <div class="features-item">
-                  <div class="features-icon"><span class="icon-map"></span></div>
-                  <h3 class="features-title font-alt">Central Location</h3>Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.
-                </div>
-              </div>
-              <div class="col-sm-6 col-md-3 col-lg-3">
-                <div class="features-item">
-                  <div class="features-icon"><span class="icon-heart"></span></div>
-                  <h3 class="features-title font-alt">High quality</h3>Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.
-                </div>
-              </div>
+            
+              
             </div>
           </div>
         </section>
@@ -76,188 +66,42 @@ require_once("menu.php");
           <div class="container">
             <div class="row">
               <div class="col-sm-6 col-sm-offset-3">
-                <h2 class="module-title font-alt mb-0">The Best Restaurant In Town. Enjoy Your Meal</h2>
+                <h2 class="module-title font-alt mb-0">The Best Writing Center. Enjoy Our Professional Services</h2>
               </div>
             </div>
           </div>
-          <div class="video-player" data-property="{videoURL:'https://www.youtube.com/watch?v=i_XV7YCRzKo', containment:'.module-video', startAt:3, mute:true, autoPlay:true, loop:true, opacity:1, showControls:false, showYTLogo:false, vol:25}"></div>
+          <div class="video-player" data-property="{videoURL:'https://www.youtube.com/watch?v=1ex_bNIFR1A&t=225s', containment:'.module-video', startAt:60, mute:true, autoPlay:true, loop:true, opacity:1, showControls:false, showYTLogo:false, vol:25}"></div>
         </section>
-        <section class="module" id="menu">
-          <div class="container">
-            <div class="row">
-              <div class="col-sm-2 col-sm-offset-5">
-                <div class="alt-module-subtitle"><span class="holder-w"></span>
-                  <h5 class="font-serif">Most popular menu</h5><span class="holder-w"></span>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-sm-8 col-sm-offset-2">
-                <h2 class="module-title font-alt">Popular Dishes</h2>
-              </div>
-            </div>
-            <div class="row multi-columns-row">
-              <div class="col-sm-6">
-                <div class="menu">
-                  <div class="row">
-                    <div class="col-sm-8">
-                      <h4 class="menu-title font-alt">Wild Mushroom Bucatini with Kale</h4>
-                      <div class="menu-detail font-serif">Mushroom / Veggie / White Sources</div>
-                    </div>
-                    <div class="col-sm-4 menu-price-detail">
-                      <h4 class="menu-price font-alt">$10.5</h4>
-                    </div>
-                  </div>
-                </div>
-                <div class="menu">
-                  <div class="row">
-                    <div class="col-sm-8">
-                      <h4 class="menu-title font-alt">Lemon and Garlic Green Beans</h4>
-                      <div class="menu-detail font-serif">Lemon / Garlic / Beans</div>
-                    </div>
-                    <div class="col-sm-4 menu-price-detail">
-                      <h4 class="menu-price font-alt">$14.5</h4>
-                    </div>
-                  </div>
-                </div>
-                <div class="menu">
-                  <div class="row">
-                    <div class="col-sm-8">
-                      <h4 class="menu-title font-alt">LambBeef Kofka Skewers with Tzatziki</h4>
-                      <div class="menu-detail font-serif">Lamb / Wine / Butter</div>
-                    </div>
-                    <div class="col-sm-4 menu-price-detail">
-                      <h4 class="menu-price font-alt">$18.5</h4>
-                    </div>
-                  </div>
-                </div>
-                <div class="menu">
-                  <div class="row">
-                    <div class="col-sm-8">
-                      <h4 class="menu-title font-alt">Imported Oysters Grill (5 Pieces)</h4>
-                      <div class="menu-detail font-serif">Oysters / Veggie / Ginger</div>
-                    </div>
-                    <div class="col-sm-4 menu-price-detail">
-                      <h4 class="menu-price font-alt">$15.9</h4>
-                    </div>
-                  </div>
-                </div>
-                <div class="menu">
-                  <div class="row">
-                    <div class="col-sm-8">
-                      <h4 class="menu-title font-alt">Meatloaf with Black Pepper-Honey BBQ</h4>
-                      <div class="menu-detail font-serif">Pepper / Chicken / Honey</div>
-                    </div>
-                    <div class="col-sm-4 menu-price-detail">
-                      <h4 class="menu-price font-alt">$16.4</h4>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-sm-6">
-                <div class="menu">
-                  <div class="row">
-                    <div class="col-sm-8">
-                      <h4 class="menu-title font-alt">Wild Mushroom Bucatini with Kale</h4>
-                      <div class="menu-detail font-serif">Mushroom / Veggie / White Sources</div>
-                    </div>
-                    <div class="col-sm-4 menu-price-detail">
-                      <h4 class="menu-price font-alt">$10.5</h4>
-                    </div>
-                  </div>
-                </div>
-                <div class="menu">
-                  <div class="row">
-                    <div class="col-sm-8">
-                      <h4 class="menu-title font-alt">Lemon and Garlic Green Beans</h4>
-                      <div class="menu-detail font-serif">Lemon / Garlic / Beans</div>
-                    </div>
-                    <div class="col-sm-4 menu-price-detail">
-                      <h4 class="menu-price font-alt">$14.5</h4>
-                    </div>
-                  </div>
-                </div>
-                <div class="menu">
-                  <div class="row">
-                    <div class="col-sm-8">
-                      <h4 class="menu-title font-alt">LambBeef Kofka Skewers with Tzatziki</h4>
-                      <div class="menu-detail font-serif">Lamb / Wine / Butter</div>
-                    </div>
-                    <div class="col-sm-4 menu-price-detail">
-                      <h4 class="menu-price font-alt">$18.5</h4>
-                    </div>
-                  </div>
-                </div>
-                <div class="menu">
-                  <div class="row">
-                    <div class="col-sm-8">
-                      <h4 class="menu-title font-alt">Imported Oysters Grill (5 Pieces)</h4>
-                      <div class="menu-detail font-serif">Oysters / Veggie / Ginger</div>
-                    </div>
-                    <div class="col-sm-4 menu-price-detail">
-                      <h4 class="menu-price font-alt">$15.9</h4>
-                    </div>
-                  </div>
-                </div>
-                <div class="menu">
-                  <div class="row">
-                    <div class="col-sm-8">
-                      <h4 class="menu-title font-alt">Meatloaf with Black Pepper-Honey BBQ</h4>
-                      <div class="menu-detail font-serif">Pepper / Chicken / Honey</div>
-                    </div>
-                    <div class="col-sm-4 menu-price-detail">
-                      <h4 class="menu-price font-alt">$16.4</h4>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="text-center"><a class="btn btn-border-d mt-50" href="restaurant_menu1.html">Check our full menu</a></div>
-          </div>
-        </section>
-        <section class="module pt-0 pb-0">
+        
+        <section class="module pt-0 pb-0" id="quote">
           <div class="row position-relative m-0">
             <div class="col-xs-12 col-md-6 side-image-text">
               <div class="row">
                 <div class="col-sm-12">
-                  <h2 class="module-title font-alt align-left">Reserve For Writing Service</h2>
-                  <p class="module-subtitle font-serif align-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+                  <h2 class="module-title font-alt align-left">Get an Instant Quote and Place Your Order</h2>
                 </div>
               </div>
-              <form class="reservation-form" id="reservationForm" action="php/reservation.php" method="post">
+            
+              <form class="quote-form" id="quoteForm" action="php/reservation.php" method="post">
                 <div class="form-group">
                   <div class="input-group">
-                    <div class="input-group-addon"><i class="icon-calendar"></i></div>
-                    <input class="form-control input-lg" id="date" type="date" name="date" placeholder="dd/mm/yyyy" required=""/>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <div class="input-group">
-                    <div class="input-group-addon"><i class="icon-clock"></i></div>
-                    <select class="form-control input-lg" id="time" name="time" type="text" required="required">
-                      <option value="time" disabled="" selected="">Select Time</option>
-                      <option value="1.30">1:30 pm</option>
-                      <option value="3.00">3:00 pm</option>
-                      <option value="4.30">4:30 pm</option>
-                      <option value="6.00">6:00 pm</option>
-                      <option value="7.30">7:30 pm</option>
+                    <div class="input-group-addon"><i class="icon-edit"></i></div>
+                    <select class="form-control input-lg" id="service_type" name="service_type" type="text" required="required">
+                      <option value="service" disabled="" selected="">Select Writing Service</option>
+                      <option value="academic">Academic</option>
+                      <option value="professional">Professional</option>
                     </select>
                   </div>
                 </div>
                 <div class="form-group">
                   <div class="input-group">
-                    <div class="input-group-addon"><i class="icon-profile-male"></i></div>
-                    <input class="form-control input-lg" id="people" type="number" name="people" min="1" max="50" step="1" placeholder="Number of people" required=""/>
+                  <div class="input-group-addon"><i class="icon-upload"></i></div>
+                    <input class="form-control input-lg" id="doc-upload" type="file" name="doc-upload"  placeholder="Upload your Document" required="required"/>
                   </div>
                 </div>
+                
                 <div class="form-group">
-                  <div class="input-group">
-                    <div class="input-group-addon"><i class="icon-envelope"></i></div>
-                    <input class="form-control input-lg" type="email" id="email" name="email" placeholder="Your Email*" required="required" data-validation-required-message="Please enter your email address."/>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <button class="btn btn-g btn-round btn-block btn-lg mt-20" id="rfsubmit" type="submit"><i class="fa fa-paper-plane-o"></i> Reserve</button>
+                  <button class="btn btn-g btn-round btn-block btn-lg mt-20" id="getquote" type="submit"><i class="fa fa-paper-plane-o"></i> Get a Quote</button>
                 </div>
                 <div id="reservationFormResponse"></div>
               </form>
@@ -265,9 +109,7 @@ require_once("menu.php");
             <div class="col-xs-12 col-md-6 col-md-offset-6 side-image restaurant-image-overlay" data-background="assets/images/restaurant/reservation.jpg"></div>
           </div>
         </section>
-        <section id="map-section">
-          <div id="map"></div>
-        </section>
+       
         <?php
         require_once("footer.php"); 
         ?>
